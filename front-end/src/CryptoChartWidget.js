46fc1d8fb6932026458eed23cd5f4f01a0d2692b
Bradley Butler <bradley@Bradleys-MacBook-Pro.local>
2024-02-01 18:01:24 -0700

    update chart widget

--- front-end/src/CryptoChartWidget.js
+++ front-end/src/CryptoChartWidget.js
@@ -4,7 +4,11 @@ const CryptoChartWidget = () => {
 // Had to pay for TradingView websocket so it's just a rectangle
 
     return (
-            <div className='mx-auto' style={{width: '50rem', height: '30rem', backgroundColor: 'black'}}></div>
+            <div className='mx-auto d-flex align-items-center' style={{width: '50rem', height: '30rem', backgroundColor: 'black'}}>
+                <div className='text-center px-5' style={{color: 'white', fontSize: '2rem'}}>
+                    Chart API was charging money for the data so it's just this rectangle, sorry!
+                </div>
+            </div>
     )
 }
 
